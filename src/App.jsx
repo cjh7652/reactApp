9909fb3ea3c55623d045df220e1b6d6fc1d8e443
jeehyun <cjh7652@hanmail.net>
2026-05-26 18:57:57 +0900

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,55 @@
-import {useState} from 'react'
+import { useState} from 'react'
 import './App.scss'
-import MyComponent from './MyComponent'
+import Modal from './components/Modal'
+
 function App() {
-    let post="가산디지털단지 맛집"
-    const [message, setMessage] = useState('안녕')
+    
+    const [blog, setBlog] = useState(['가산디지털단지 맛집','강남 맛집','신림맛집','피자','콜라','호식이' ])
+    const [cnt, setCnt] = useState(0)
+    const [modal, setModal] = useState(false)
+ 
+
   return (
     <div className='App'>
         <div className="black-nav">
             <h4>블로그 만들거예요</h4>
         </div>
-        <div className="blogBody">
-            <h4 style={{fontSize:'18px'}}>{post}</h4>
-
-            <MyComponent name="리액트" a="바보"/>
+        <button onClick={() => {
+            let copy=[...blog];
+            copy[0] ='구로맛집';
+            setBlog(copy)
+        }}>
+            글수정
+        </button>
+        <div className="list">
+            <h4>{blog[0]} <span onClick={()=>{setCnt(cnt+1)}}>👍</span>{cnt}</h4>
+            <h4>글제목</h4>
+            <p>5월 26일 발행</p> 
         </div>
-        <button onClick={()=>setMessage('어서오세요')}>입장</button>
-        <button onClick={()=>setMessage('안녕히 가세요')}>퇴장</button>
-        <h2>{message}</h2>
-        <p>dksldkfjlws</p>
+        <div className="list">
+            <h4>{blog[1]}</h4>
+            <h4>글제목</h4>
+            <p>5월 26일 발행</p> 
+        </div>
+        <div className="list">
+            <h4>{blog[2]}</h4>
+            <h4 onClick={() =>{setModal(!modal)}}>글제목</h4>
+            <p>5월 26일 발행</p> 
+        </div>
+       {
+         modal==true ? <Modal/> : null
+       }
+       {
+        blog.map(function(a){
+            return (
+                 <div className="list">
+                    <h4>{a}</h4>
+                    <h4 onClick={() =>{setModal(!modal)}}>글제목</h4>
+                    <p>5월 26일 발행</p> 
+                </div>
+            )
+        })
+       }
     </div>
   )
 }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Modal = () => {
+    return (
+        <div className='modal'>
+            <h2>제목</h2>
+            <p>날짜</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus id, rerum exercitationem animi molestias possimus libero nulla? Quod voluptatibus ex atque a, assumenda magni esse autem iusto quia, consequuntur accusantium?</p>
+        </div>
+    );
+};
+
+export default Modal;
